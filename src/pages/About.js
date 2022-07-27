33c614ea5911e4c99c1b8b49f5ba934d2411bab0
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-27 15:01:31 +0300

    add description

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,9 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">Contact Us</h1>
+      <p className="section-description">
+        This is a top-notch delicacy firm that is committed to serving you with fresh cocktail juices as well as giving you preparation methods for each drink. You can reach us through any of the contacts below. Follow us on our social media platforms as well to continue receiving more updates.
+      </p>
       <p>
        <a href="http://www.facebook.com/contact">Facebook</a> <br/>
        <a href="http://www.facebook.com/contact">Twitter</a> <br/>
