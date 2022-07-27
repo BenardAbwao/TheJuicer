756aeeb60ac883c346963561d0b8b6de41f79bc8
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-27 21:30:45 +0300

    update links

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faLinkedin,
+  faTwitter,
+  faInstagram,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
+
 
 const About = () => {
   return (
@@ -7,11 +16,34 @@ const About = () => {
       <p className="section-description">
         This is a top-notch delicacy firm that is committed to serving you with fresh cocktail juices as well as giving you preparation methods for each drink. You can reach us through any of the contacts below. Follow us on our social media platforms as well to continue receiving more updates.
       </p>
-      <p className= "section-links">
-       <a href="http://www.facebook.com/contact">Facebook</a> <br/>
-       <a href="http://www.facebook.com/contact">Twitter</a> <br/>
-       <a href="http://www.facebook.com/contact">Instagram</a>
-      </p>
+
+      <div className="section-contacts">
+            <a href="https://github.com/BenardAbwao"
+        className="github social">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      
+      <a href="https://www.linkedin.com/in/benard-abwao-533a2a180"
+        className="linkedin social">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+
+      <a href="https://twitter.com/BenardAbwao2" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+
+      <a href="https://www.instagram.com/benard.abwao"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+
+      <a href="https://www.facebook.com/benard.abwao5"
+        className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+          
+          
+      </div>
     </section>
   )
 }
