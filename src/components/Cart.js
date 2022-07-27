5bd914149ca9e9e7d9d47052ed8b1f7ba675ad75
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-28 00:45:47 +0300

    add cart

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Cart = () => {
+    return (
+      <section className="section order-section">
+        <h1 className="section-title">We will soon start accepting orders</h1>
+      </section>
+    )
+  }
+  
+  export default Cart;
