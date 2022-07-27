d9a66190ce2c3b5fd94ce17dee7c9e2f91d2262c
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-27 14:19:10 +0300

    add order component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleCocktail from "./pages/SingleCocktail";
 import Navbar from "./components/Navbar";
+import Order from "./pages/Order"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/cocktail/:Id" element={<SingleCocktail />} />
+          <Route path="/order" element={<Order />} />
         </Routes>
       </Router>
     </div>
