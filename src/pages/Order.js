3e021566693bf90052cd80b2f8e7719e627a1376
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-27 14:17:50 +0300

    add order page

---
+++ src/pages/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Order = () => {
+    return (
+      <section className="section order-section">
+        <h1 className="section-title">Order Now</h1>
+      </section>
+    )
+  }
+  
+  export default Order
