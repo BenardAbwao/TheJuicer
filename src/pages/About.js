7cd3023d9fc8396ef01959628ad3de3c103af3fc
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-27 08:17:50 +0300

    add media links for contact

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">Contact Us</h1>
+      <p>
+       <a href="http://www.facebook.com/contact">Facebook</a> <br/>
+       <a href="http://www.facebook.com/contact">Twitter</a> <br/>
+       <a href="http://www.facebook.com/contact">Instagram</a>
+      </p>
+    </section>
+  )
+}
+
+export default About
