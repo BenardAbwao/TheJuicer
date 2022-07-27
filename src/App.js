80dd662dd03f434e71bade13402e0f36d89658c7
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-28 00:47:57 +0300

    create cart path

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleCocktail from "./pages/SingleCocktail";
 import Navbar from "./components/Navbar";
-import Order from "./pages/Order"
+import Cart from "./components/Cart";
 
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/cocktail/:Id" element={<SingleCocktail />} />
-          <Route path="/order" element={<Order />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </div>
