b8d53ff9428a09f4a58516ebaa46f88991840a42
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-30 01:46:53 +0300

    update signup route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleCocktail from "./pages/SingleCocktail";
 import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
+import SignUp from "./components/SignUp";
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/" element={<Home/>}></Route>
           <Route exact path="/cocktail/:Id" element={<SingleCocktail/>}></Route>
           <Route exact path="/about" element={<About/>}></Route>
-          <Route exact path="/cart" element={<Cart/>}></Route>
+          <Route exact path="/signup" element={<SignUp/>}></Route>
         </Routes>
       </Router>
   );
