51d93f78ad9cf20bea0990b0d2dc3bef4c213269
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-30 01:42:36 +0300

    import link

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
+import { Link } from "react-router-dom";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
@@ -64,6 +65,9 @@ const SingleCocktail = () => {
                 <span> {strIngredient2}</span>,
                 <span> {strIngredient3}</span>
               </p>
+              <center>
+              <Link className='btn btn-primary btn-order' to={'/signup/'}>Order Now</Link>
+              </center>
             </div>
           </div>
         </section>
